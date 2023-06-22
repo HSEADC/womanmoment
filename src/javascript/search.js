5e47e069735bea2054ccc7ba721fe996045ff60a
Exsi212 <exsi112@gmail.com>
2023-06-22 09:35:33 +0300

    ss

--- src/javascript/search.js
+++ src/javascript/search.js
@@ -1,8 +1,18 @@
 const resultLinks = [
   { name: 'История 8 марта', href: '/historyofmart.html' },
-  { name: 'Семиотика кухни', href: '/semiotickitchen.html' },
+  { name: 'Семиотика кухни', href: '/artpieces/artpiece1.html' },
   { name: 'Бетти Фридан', href: '/activity.html' },
   { name: 'Смелая', href: '/book2.html' },
+  { name: 'Загадка женственности', href: '/book1.html' },
+  { name: 'Fem Talks', href: '/podcast.html' },
+  { name: 'Революция и феминизм', href: '/revolution.html' },
+  { name: 'Абигейл Смит Адамс', href: '/adams.html' },
+  { name: 'Суфражизм', href: '/sufragette.html' },
+  { name: 'Ана Мендиета', href: '/ana-mendieta.html' },
+  { name: 'Арт-феминизм', href: '/art-feminism.html' },
+  { name: 'Яеи Кусама', href: '/kusama.html' },
+  { name: 'Кэроли Шнееманн', href: '/sheeman.html' },
+  { name: 'Работы Синди Шерман', href: '/sherman.html' }
   // массив
 ]
 
